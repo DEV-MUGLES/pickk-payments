8e117e85cf9dabf2154c045412d1eba4db7442f5
greatSumini <greatSumini@gmail.com>
2021-07-06 00:05:46 +0900

    fix(server): :wrench: fix env.URL

--- services/pickk-pay-server/src/main.ts
+++ services/pickk-pay-server/src/main.ts
@@ -1,6 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
+import { AppConfigService } from '@config/app/config.service';
+
 import { AppModule } from './app.module';
+
+const APP_CONFIG_SERVICE = 'AppConfigService';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +13,7 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  const appConfig: AppConfigService = app.get(APP_CONFIG_SERVICE);
+  await app.listen(appConfig.port);
 }
 bootstrap();
