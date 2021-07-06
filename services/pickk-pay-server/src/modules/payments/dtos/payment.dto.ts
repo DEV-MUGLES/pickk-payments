f0b12dbfb084d5d4ba6aa167d02f695fd8a8c88d
greatSumini <greatSumini@gmail.com>
2021-07-06 18:41:47 +0900

    refactor(server): :recycle: add factory method to PaymentListResponseDto

--- services/pickk-pay-server/src/modules/payments/dtos/payment.dto.ts
+++ services/pickk-pay-server/src/modules/payments/dtos/payment.dto.ts
@@ -1,4 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Pg, PaymentStatus, PayMethod } from '@pickk/pay';
+
 import { Payment } from '@payments/entities/payment.entity';
 
 export class PaymentListResponseDto {
@@ -40,4 +42,59 @@ export class PaymentListResponseDto {
   };
 
   payments: Payment[];
+
+  public static of(payments: Payment[]): PaymentListResponseDto {
+    const result = new PaymentListResponseDto();
+
+    const pgCount = {
+        [Pg.Inicis]: 0,
+      },
+      statusCount = {
+        [PaymentStatus.Ready]: 0,
+        [PaymentStatus.Paid]: 0,
+        [PaymentStatus.Failed]: 0,
+        [PaymentStatus.Cancelled]: 0,
+        [PaymentStatus.PartialCancelled]: 0,
+      },
+      payMethodCount = {
+        [PayMethod.Card]: 0,
+        [PayMethod.Vbank]: 0,
+        [PayMethod.Trans]: 0,
+        [PayMethod.Kakaopay]: 0,
+      },
+      amounts = {
+        totalPaidAmount: 0,
+        totalCancelledAmount: 0,
+      };
+
+    payments.forEach((payment) => {
+      const { pg, status, payMethod, amount } = payment;
+
+      pgCount[pg] ? (pgCount[pg] += 1) : (pgCount[pg] = 1);
+      statusCount[status]
+        ? (statusCount[status] += 1)
+        : (statusCount[status] = 1);
+      payMethodCount[payMethod]
+        ? (payMethodCount[payMethod] += 1)
+        : (payMethodCount[payMethod] = 1);
+
+      const { Ready, Failed, Cancelled, PartialCancelled } = PaymentStatus;
+
+      if (status !== Ready && status !== Failed) {
+        amounts.totalPaidAmount += amount;
+      }
+
+      if (status === Cancelled || status === PartialCancelled) {
+        amounts.totalCancelledAmount += amount;
+      }
+    });
+
+    result.pgCount = pgCount;
+    result.payMethodCount = payMethodCount;
+    result.statusCount = statusCount;
+    result.amounts = amounts;
+    result.payments = payments;
+
+    return result;
+  }
 }
--- services/pickk-pay-server/src/modules/payments/payments.controller.ts
+++ services/pickk-pay-server/src/modules/payments/payments.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Inject, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { PayMethod, Pg, PaymentStatus } from '@pickk/pay';
 
 import { PageParams } from '@common/dtos/pagination.dto';
 
@@ -24,55 +23,6 @@ export class PaymentsController {
       pageParams,
       ['cancellations'],
     );
-    const pgCount = {
-        [Pg.Inicis]: 0,
-      },
-      statusCount = {
-        [PaymentStatus.Ready]: 0,
-        [PaymentStatus.Paid]: 0,
-        [PaymentStatus.Failed]: 0,
-        [PaymentStatus.Cancelled]: 0,
-        [PaymentStatus.PartialCancelled]: 0,
-      },
-      payMethodCount = {
-        [PayMethod.Card]: 0,
-        [PayMethod.Vbank]: 0,
-        [PayMethod.Trans]: 0,
-        [PayMethod.Kakaopay]: 0,
-      };
-    const amounts = {
-      totalPaidAmount: 0,
-      totalCancelledAmount: 0,
-    };
-
-    payments.forEach((payment) => {
-      const { pg, status, payMethod, amount } = payment;
-
-      pgCount[pg] ? (pgCount[pg] += 1) : (pgCount[pg] = 1);
-      statusCount[status]
-        ? (statusCount[status] += 1)
-        : (statusCount[status] = 1);
-      payMethodCount[payMethod]
-        ? (payMethodCount[payMethod] += 1)
-        : (payMethodCount[payMethod] = 1);
-
-      const { Ready, Failed, Cancelled, PartialCancelled } = PaymentStatus;
-
-      if (status !== Ready && status !== Failed) {
-        amounts.totalPaidAmount += amount;
-      }
-
-      if (status === Cancelled || status === PartialCancelled) {
-        amounts.totalCancelledAmount += amount;
-      }
-    });
-
-    return {
-      pgCount,
-      statusCount,
-      payMethodCount,
-      amounts,
-      payments,
-    };
+    return PaymentListResponseDto.of(payments);
   }
 }
