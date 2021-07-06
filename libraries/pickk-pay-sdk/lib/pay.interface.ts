4ecafddb92776b72739506662b4a8124359a4754
syoung125 <laura120555@gmail.com>
2021-07-06 15:44:04 +0900

    feat: :label: add PaymentStatus enum

--- libraries/pickk-pay-sdk/lib/pay.interface.ts
+++ libraries/pickk-pay-sdk/lib/pay.interface.ts
@@ -4,6 +4,19 @@ export enum Pg {
   Inicis = 'inicis',
 }
 
+export enum PaymentStatus {
+  /** 미결제 */
+  Ready = 'ready',
+  /** 결제완료 */
+  Paid = 'paid',
+  /** 전액취소 */
+  Cancelled = 'cancelled',
+  /** 부분취소 */
+  PartialCancelled = 'partial_cancelled',
+  /** 결제실패 */
+  Failed = 'failed',
+}
+
 export enum PayMethod {
   /** 신용카드 */
   Card = 'card',
