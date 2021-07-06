db17deade42a61941c37605f2d2ba54c9fa733ba
greatSumini <greatSumini@gmail.com>
2021-07-06 16:46:22 +0900

    feat(sdk): :label: add IPayment

--- libraries/pickk-pay-sdk/lib/pay.interface.ts
+++ libraries/pickk-pay-sdk/lib/pay.interface.ts
@@ -1,4 +1,4 @@
-import { InicisBankCode, MobpayRequestParams } from 'inicis';
+import { InicisBankCode, InicisCardCode, MobpayRequestParams } from 'inicis';
 
 export enum Pg {
   Inicis = 'inicis',
@@ -210,3 +210,45 @@ export type PayCancelResult = Pick<
 > & {
   success: boolean;
 };
+
+export interface IPayment {
+  id: number;
+  /** 고유 주문번호 */
+  merchantUid: string;
+  status: PaymentStatus;
+
+  pg: Pg;
+  /** pg사 고유 거래번호 */
+  pgTid: string;
+  payMethod: PayMethod;
+
+  /** 주문명 */
+  name: String;
+  /** 거래 금액 */
+  amount: number;
+
+  buyerName: string;
+  buyerTel: string;
+  buyerEmail: string;
+  buyerAddr: string;
+  buyerPostalcode: string;
+
+  /** 카드사 거래 번호 */
+  applyNum?: string;
+  /** 카드사 코드 */
+  cardCode?: InicisCardCode;
+  /** 카드 번호 */
+  cardNum?: string;
+
+  vbankCode?: InicisBankCode;
+  vbankName?: string;
+  vbankNum?: string;
+  vbankHolder?: string;
+  vbankDate?: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+  failedAt?: Date;
+  paidAt?: Date;
+  cancelledAt?: Date;
+}
