41706e9f27b8b5fb2f0f3400939285a6999299ee
greatSumini <greatSumini@gmail.com>
2021-07-07 14:49:23 +0900

    feat(server): :sparkles: add vbank noti guards

--- services/pickk-pay-server/src/modules/inicis/exceptions/iniapi.exceptions.ts
+++ services/pickk-pay-server/src/modules/inicis/exceptions/iniapi.exceptions.ts
@@ -1,7 +1,16 @@
-import { InternalServerErrorException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 export class InicisCancelFailedException extends InternalServerErrorException {
   constructor(resultMsg: string) {
     super('KG이니시스 취소실패: ' + resultMsg);
+  }
+}
+
+export class NotFromInicisException extends ForbiddenException {
+  constructor(funcName: string) {
+    super(`[${funcName}] 이니시스 PG로부터의 요청만 허용됩니다.`);
   }
 }
--- services/pickk-pay-server/src/modules/inicis/inicis.controller.ts
+++ services/pickk-pay-server/src/modules/inicis/inicis.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+
+import { StdVbankNotiGuard, MobVbankNotiGuard } from './guards';
 
 @Controller('/inicis')
-export class InicisController {}
+export class InicisController {
+  @UseGuards(StdVbankNotiGuard)
+  @Post('/std/vbank-noti')
+  async stdVbankNoti(): Promise<string> {
+    return 'hi';
+  }
+
+  @UseGuards(MobVbankNotiGuard)
+  @Post('/mob/vbank-noti')
+  async mobVbankNoti(): Promise<string> {
+    return 'hi';
+  }
+}
